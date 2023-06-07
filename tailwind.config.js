183ce8571a324ca478e4fe922fe37a37f5634998
Torleif Berger <torleif@outlook.com>
2023-06-07 21:46:12 +0200

    Added `@tailwindcss/forms`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './content/**/*.mdx'],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
   theme: {
     container: {
       center: true,
@@ -53,5 +54,4 @@ module.exports = {
       }),
     },
   },
-  plugins: [require('@tailwindcss/typography')],
 }
