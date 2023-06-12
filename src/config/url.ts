32f2eb74fbd1ac883c25b7a1f559109c48cb5931
Torleif Berger <torleif@outlook.com>
2023-06-12 18:35:13 +0200

    Default PORT

--- src/config/url.ts
+++ src/config/url.ts
@@ -4,7 +4,7 @@ export const BASE_URL = process.env.PRIMARY_URL
   ? `https://${process.env.PRIMARY_URL}`
   : process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : `http://localhost:${process.env.PORT}`
+  : `http://localhost:${process.env.PORT ?? 3000}`
 
 /**
  * Returns the absolute URL for the given pathname.
