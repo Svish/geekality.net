1b4d3e7e1f2fb8c503d26d4361e69b7df2d110fd
Torleif Berger <torleif@outlook.com>
2024-01-03 22:32:37 +0100

    Added `@next/bundle-analyzer`

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withBundleAnalyzer from '@next/bundle-analyzer'
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -9,4 +11,6 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(nextConfig)
