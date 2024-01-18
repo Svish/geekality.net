659dfcc9e7dee9a1354ddd6d5c06a1dc27e44dd9
Torleif Berger <torleif@outlook.com>
2024-01-18 11:38:35 +0100

    Added some security headers

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,27 @@ const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
+  headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default withBundleAnalyzer({
